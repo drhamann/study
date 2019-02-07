fda5598c73c6e925f1fb6da356c9cfbb0b9f574c
George <george.hamann@gmail.com>
2019-02-06 23:47:00 -0200

    MVVM training SPA

---
+++ MVVM.Traininig/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MVVM.Training.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVVM.Traininig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TraininProductViewModel trainingManager = new TraininProductViewModel();
+            trainingManager.HandleRequest();
+            return View(trainingManager);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TraininProductViewModel viewModel)
+        {
+            viewModel.IsValid = ModelState.IsValid;
+            viewModel.HandleRequest();
+            if(viewModel.IsValid)
+                ModelState.Clear();
+
+            return View(viewModel);
+        }
+    }
+}
